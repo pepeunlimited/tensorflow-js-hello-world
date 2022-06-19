18ec031a8b917d1103757492bfee329df0959bb9
Simo Ala-Kotila <simo.alakotila@gmail.com>
2022-06-19 13:25:42 +0300

    Human type for tf

--- dist/data-node/data.js
+++ dist/data-node/data.js
@@ -17,28 +17,76 @@ const DatasetV1 = () => {
     // const data: tf.TensorContainer[] = [
     //   1, 2, 3
     // ]
-    const data1 = [
+    //
+    // const xs = Object.keys(human).map((key) => {
+    // const value = human[key as keyof typeof human]
+    // switch (key) {
+    // case 'sex':
+    // switch (value) {
+    // case 'M':
+    // return 0
+    // case 'F':
+    // return 1
+    // default:
+    // return -1
+    // }
+    // default:
+    // return value
+    // }
+    // })
+    //
+    // const xs = Object.fromEntries(['age', 'weight', 'height'].map(k => [k, human[k as keyof typeof human]]))
+    const data2 = [
         {
-            xs: 1,
-            ys: 58,
+            sex: 'M',
+            age: 30,
+            weight: 70,
+            height: 180,
         },
         {
-            xs: 1,
-            ys: 88,
+            sex: 'F',
+            age: 28,
+            weight: 60,
+            height: 179,
         },
         {
-            xs: 0,
-            ys: 44,
+            sex: 'F',
+            age: 25,
+            weight: 50,
+            height: 169,
         },
     ];
-    const dataset = tf.data.array(data1);
+    const dataset = tf.data.array(data2);
     return {
         dataset: dataset.map((row) => {
             console.log(`dataset.row=${JSON.stringify(row)}`);
-            const xy = row;
-            return { xs: [xy.xs], ys: [xy.ys] };
+            const human = row;
+            // NOTICE:
+            // with tf.data.csv you should use for FEATURE(xs) and LABEL(ys)
+            // const xs = row['xs'] 
+            // const ys = row['ys']
+            // filter sex out of the human row
+            const xs = Object.keys(human)
+                .filter((k) => !k.includes('sex'))
+                .map((k) => human[k]);
+            // filter sex in of the human row
+            const ys = Object.keys(human)
+                .filter((k) => k.includes('sex'))
+                .map((k) => {
+                // typecast sex M(0) F(1)
+                const sex = human[k];
+                switch (sex) {
+                    case 'M':
+                        return 0;
+                    case 'F':
+                        return 1;
+                    default:
+                        return -1;
+                }
+            });
+            return { xs: xs, ys: ys };
         }),
-        numberOfColumns: 1, // @see data-node/model.ts inputShape & units
+        numberOfColumns: 3, // @see Data.numberOfColumns
     };
 };
 exports.DatasetV1 = DatasetV1;
--- src/data-node/data.ts
+++ src/data-node/data.ts
@@ -1,16 +1,23 @@
 import * as tf from '@tensorflow/tfjs-node'
 
-type XY = {
-  // sex of the given object (feature)
-  // male    = 0
-  // female  = 1 
-  xs: number
-  // age of the given object (label)
-  ys: number
+type Sex = 'M' | 'F'
+
+type Human = {
+  // sex of the given object (tf.label)
+  sex: Sex
+  // age of the given object (tf.feature)
+  age: number
+  // weight of the given object (tf.feature)
+  weight: number
+  // height of the given object (tf.feature)
+  height: number
 }
 
 type Data = {
   dataset: tf.data.Dataset<tf.TensorContainer>
+  // @see data-node/data.ts  > Object.keys(human) - sex
+  // @see data-node/model.ts > inputShape & units
+  // @see data-node/train.ts > CreateModel([data.numberOfColumns])
   numberOfColumns: number
 }
 
@@ -29,35 +36,80 @@ const DatasetV1 = (): Data => {
   // const data: tf.TensorContainer[] = [
   //   1, 2, 3
   // ]
-  // 
-  // @see 
-  //  
-  //  switch (rawFeatures[key]) {
-  //  default:
-  //  return Number(rawFeatures[key]); <- see this one
   //
-  const data1: XY[] = [
+  // const xs = Object.keys(human).map((key) => {
+  // const value = human[key as keyof typeof human]
+  // switch (key) {
+  // case 'sex':
+  // switch (value) {
+  // case 'M':
+  // return 0
+  // case 'F':
+  // return 1
+  // default:
+  // return -1
+  // }
+  // default:
+  // return value
+  // }
+  // })
+  //
+  // const xs = Object.fromEntries(['age', 'weight', 'height'].map(k => [k, human[k as keyof typeof human]]))
+
+  const data2: Human[] = [
     {
-      xs: 1,
-      ys: 58,
+      sex: 'M',
+      age: 30,
+      weight: 70,
+      height: 180,
     },
     {
-      xs: 1,
-      ys: 88,
+      sex: 'F',
+      age: 28,
+      weight: 60,
+      height: 179,
     },
     {
-      xs: 0,
-      ys: 44,
+      sex: 'F',
+      age: 25,
+      weight: 50,
+      height: 169,
     },
   ]
-  const dataset: tf.data.Dataset<XY> = tf.data.array(data1)
+
+  const dataset: tf.data.Dataset<Human> = tf.data.array(data2)
   return {
     dataset: dataset.map((row) => {
       console.log(`dataset.row=${JSON.stringify(row)}`)
-      const xy: XY = row as XY
-      return { xs: [xy.xs], ys: [xy.ys] }
+      const human: Human = row as Human
+      
+      // NOTICE:
+      // with tf.data.csv you should use for FEATURE(xs) and LABEL(ys)
+      // const xs = row['xs'] 
+      // const ys = row['ys']
+
+      // filter sex out of the human row
+      const xs = Object.keys(human)
+        .filter((k) => !k.includes('sex'))
+        .map((k) => human[k as keyof typeof human])
+      // filter sex in of the human row
+      const ys = Object.keys(human)
+        .filter((k) => k.includes('sex'))
+        .map((k) => {
+          // typecast sex M(0) F(1)
+          const sex = human[k as keyof typeof human]
+          switch (sex) {
+            case 'M':
+              return 0
+            case 'F':
+              return 1
+            default:
+              return -1
+          }
+        })
+      return { xs: xs, ys: ys }
     }),
-    numberOfColumns: 1, // @see data-node/model.ts inputShape & units
+    numberOfColumns: 3, // @see Data.numberOfColumns
   }
 }
 
