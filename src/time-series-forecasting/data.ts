a944f36b06975822708c0b7b88b1cce516965446
Simo Ala-Kotila <simo.alakotila@gmail.com>
2022-07-03 12:28:14 +0300

    time-series-forecasting

---
+++ src/time-series-forecasting/data.ts
@@ -0,0 +1,12 @@
+type JenaWeather = {
+
+}
+
+const Dataset = (): JenaWeather => {
+  // check if the .csv exists, if not download otherwise load it from disk
+  return {
+
+  }
+}
+
+export { JenaWeather, Dataset }
