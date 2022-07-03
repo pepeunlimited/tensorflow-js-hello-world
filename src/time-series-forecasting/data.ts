4b2bedf3ad3cfdffc67ba759d35e577b141f3c6a
Simo Ala-Kotila <simo.alakotila@gmail.com>
2022-07-03 13:53:19 +0300

    path & endpointURL for csv

--- src/time-series-forecasting/data.ts
+++ src/time-series-forecasting/data.ts
@@ -3,7 +3,14 @@ type JenaWeather = {
 }
 
 const Dataset = (): JenaWeather => {
+  const csv: string = 'jena_climate_2009_2016.csv'
+  // url for the external download
+  const endpointURL: string = `https://storage.googleapis.com/learnjs-data/jena_climate/${csv}`
+  // path for local
+  const path: string = `./${csv}`
+  
   // check if the .csv exists, if not download it from given URL
+  
   return {
 
   }
