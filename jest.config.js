9819034ef0bec87f10dac21d32a0fe412f73ffa1
Simo Ala-Kotila <simo.alakotila@gmail.com>
2022-07-14 23:35:18 +0300

    jest for tests

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "roots": [
+      "<rootDir>/src"
+  ],
+  "testMatch": [
+      "**/__tests__/**/*.+(ts|tsx|js)",
+      "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  // @see https://jestjs.io/docs/ecmascript-modules
+  "transform": {},
+}
