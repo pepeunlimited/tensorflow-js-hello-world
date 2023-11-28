22b55e841995d90c329b23dea714d830ae6b1805
Simo Ala-Kotila <simo.alakotila@gmail.com>
2023-11-28 10:49:36 +0200

    Remove ignored files

--- dist/time-series-forecasting/unreachable_case_error.js
+++
@@ -1,7 +0,0 @@
-// Exhaustive switch helper
-class UnreachableCaseError extends Error {
-    constructor(val) {
-        super(`Unreachable case: ${val}`);
-    }
-}
-export { UnreachableCaseError };
